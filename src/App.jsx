449d515226d7aca712c29186d812eac38a49d773
kabuzo13 <beahonigmann@gmail.com>
2024-03-05 14:02:25 +0000

    imported bootstrap, created folders for components, added navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header/header";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import home from "./components/Home/home";
+import './App.css'
+
+function App() {
+  return (
+    <Header></Header>
+  )
+}
+
+export default App
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function home() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Beatrix Honigmann</h1>
+    <p class="lead">Aspiring developer</p>
+  </div>
+</div>
+  )
+}
+
+export default home;
+
+// name, photo, brand statement, portfolio
